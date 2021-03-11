b18bdea46fdcafb4c46bbbd73be9e0b3bd8bb96e
MekhiW34 <washington.mekhi@yahoo.com>
2021-03-11 09:10:22 -0500

    I just add my demo1 file

---
+++ Testnumber1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Testnumber1 {
+    public static void main(String[] args) {
+        System.out.println("Pushed to github successfully");
+    }
+}
---
+++ Testnumber2.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Testnumber2 {
+    public static void main(String[] args) {
+        System.out.println("I got this");
+    }
+}
